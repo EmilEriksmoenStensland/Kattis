9dae5790bac5560d2a54963cf50cea89adcd6904
Emil Eriksmoen Stensland <emil.stensland@gmail.com>
2022-03-01 23:01:31 +0100

    EchoEchoEcho

---
+++ EchoEchoEcho.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class EchoEchoEcho {
+  public static void main(String[] args) {
+    Scanner key = new Scanner(System.in);
+    String echo = key.nextLine();
+    System.out.println(echo + " " + echo + " " + echo);
+  }
+}
